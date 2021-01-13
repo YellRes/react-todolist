ff541aafe7fe5b4ec0f86c0932d9b76fa8e23e02
Yellres <2929799602@qq.com>
2021-01-13 20:39:47 +0800

    fix: 项目正常显示

--- src/filter/reducer.js
+++ src/filter/reducer.js
@@ -1,6 +1,6 @@
 import {SET_FILTER} from './actionTypes'
 
-export default (state = '', action) => {
+export default (state = '0', action) => {
     switch(action.type) {
         case SET_FILTER: {
             return action.filter
